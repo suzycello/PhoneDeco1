3fbc12c7df05834ccaac0a864fca9f4cbf4e5b0b
unknown <cheonjw1370@gmail.com>
2023-12-29 20:37:10 +0900

    first commit

--- backend/api/v1/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-
-//api 경로 분류
-router.use("/user", require("./user"));
-
-module.exports = router;
--- backend/api/v1/post/middlewares.js
+++
@@ -1,6 +0,0 @@
-const post_upload = (req, res, next) =>
-  !req.user ? next(new Error("Authorization required")) : next(); //업로드할때 로그인 되어있는지 체크하는 코드
-
-const post_load = (req, res, next) => next();
-
-module.exports = { post_upload, post_load };
